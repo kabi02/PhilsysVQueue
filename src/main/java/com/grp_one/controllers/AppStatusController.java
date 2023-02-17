e3670ea60095cec245c7cbc6dac8201985787a74
Korbi <kbillones98@gmail.com>
2023-02-17 13:30:03 +0800

    added appstatus and idstatus on user side

---
+++ src/main/java/com/grp_one/controllers/AppStatusController.java
@@ -0,0 +1,16 @@
+package com.grp_one.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+
+public class AppStatusController {
+    @FXML
+    private Label lblAppStatus;
+
+    @FXML
+    private Hyperlink linkStatusDocStat;
+
+    @FXML
+    private Hyperlink linkStatusGoBack;
+}
---
+++ src/main/java/com/grp_one/controllers/IDStatusController.java
@@ -0,0 +1,21 @@
+package com.grp_one.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+
+public class IDStatusController {
+
+    @FXML
+    private Label lblAppStatus;
+
+    @FXML
+    private Label lblCTN;
+
+    @FXML
+    private Label lblIDStatus;
+
+    @FXML
+    private Hyperlink linkIDGoBack;
+
+}
